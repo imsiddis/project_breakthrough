c762972ab463d514e56601012176df32efd11e41
imsiddis <axelaarre@gmail.com>
2022-12-09 22:07:18 +0100

    Updated sid_mailscrape.py to use the new version of the mailscrape module. Also updated the README.md file to reflect the changes.

--- sid_mailscrape.py
+++ sid_mailscrape.py
@@ -9,12 +9,12 @@ def getURL():
     return url
 
 def getHTML(url):
-    html = urllib.request.urlopen(url).read()
+    html = urllib.request.urlopen(url).read() # This will read the HTML from the URL
     return html
 
 def getEmails(html):
     email_list = []
-    emails = re.findall(r'[\w\.-]+@[\w\.-]+', str(html))
+    emails = re.findall(r'[\w\.-]+@[\w\.-]+', str(html)) # This regex will find all email addresses on the page
     email_list.append(emails)
     return email_list
 
@@ -23,21 +23,51 @@ def getEmails(html):
 def removeDuplicates(emails):
     email_list = []
     for email in emails:
-        no_duplicates = list(set(email))
+        no_duplicates = list(set(email)) # This will remove duplicates from the list
         for i in no_duplicates:
             email_list.append(i)
-        return email_list
+        return email_list # This will return the list without duplicates
 
 def printEmails(emails):
     for email in emails:
         print(email)
 
+# A function to append the non-duplicate emails to a file.
+def saveEmails(emails):
+    with open("emails.txt", "a") as file:
+        for email in emails:
+            file.write(email + "\n")
+
+# A menu function to allow the user to choose what they want to do with the emails
+def menu(emails):
+    print("What would you like to do with the emails?")
+    print("1. Print emails to screen")
+    print("2. Save emails to file")
+    print("0. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        printEmails(emails)
+    elif choice == "2":
+        saveEmails(emails)
+    elif choice == "0":
+        print("Exiting...")
+        sys.exit()
+    else:
+        print("Invalid choice")
+        menu(emails)
+
 def main():
+    # Splash screen
+    print("================== MailScrape v1.0 ========================")
+    print("|                   By imSiddis                            |")
+    print("============================================================")
+    print("| This program will scrape email addresses from a website. |")
+    print("============================================================")
     url = getURL()
     html = getHTML(url)
     emails = getEmails(html)
     no_duplicates = removeDuplicates(emails)
     sorted_emails = sorted(no_duplicates)
-    printEmails(sorted_emails)
+    printEmails(sorted_emails) 
 
 main()
